75a1bd7cf7eb6763ad1341968b239fc504277dad
Daniel Grossman <d.grossman@wustl.edu>
2016-10-30 01:55:16 -0500

    oops

--- new_event5.php
+++ new_event5.php
@@ -1,7 +1,8 @@
 <?php
   require 'database5.php';
   header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
-
+  session_start();
+  
   $newEvent = $mysqli->prepare("INSERT INTO events (username, date, time, title, note, category) VALUES (?, ?, ?, ?, ?, ?)");
   $newEvent->bind_param('ssssss', $user, $date, $time, $title, $note, $category);
   $user = (string)$_SESSION['currentUser'];
